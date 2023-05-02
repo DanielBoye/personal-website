2a9d651c00606c178e021c0ee190402fc31ddd45
Daniel Boye <danielboye888@gmail.com>
2023-05-02 12:27:00 +0200

    Removing the script inside the html code

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
-
 let interval = 1;
 
-document.querySelector("h1").onmouseover = event => {  
+function startAnimation() {
   let iteration = 0;
   
   clearInterval(interval);
   
   interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
+    const h1 = document.querySelector("h1");
+    h1.innerText = h1.dataset.value
       .split("")
       .map((letter, index) => {
         if(index < iteration) {
-          return event.target.dataset.value[index];
+          return h1.dataset.value[index];
         }
       
         return letters[Math.floor(Math.random() * 29)]
       })
       .join("");
     
-    if(iteration >= event.target.dataset.value.length){ 
+    if(iteration >= h1.dataset.value.length){ 
       clearInterval(interval);
     }
     
     iteration += 1 / 3;
   }, 30);
 }
+
+window.addEventListener("load", startAnimation);
+
+document.querySelector("h1").addEventListener("mouseover", startAnimation);
